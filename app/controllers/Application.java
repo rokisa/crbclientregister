0383450e699faa591a7a4ed66a27e32bd84944b6
Roy Kisaka <roy.kisaka@gmail.com>
2015-02-25 14:54:57 +0300

    refactored the upload path of the uploaded photos from the android application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,21 +5,23 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.Maps;
 import models.ClientDetails;
 import models.NextOfKin;
+import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONObject;
 import play.*;
+import play.data.Form;
 import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.*;
 
 public class Application extends Controller {
 
@@ -32,9 +34,6 @@ public class Application extends Controller {
         jsnObj.put("status", "SUCCESS");
 
         JsonNode json = request().body().asJson();
-
-        System.out.print("New Values.....    "+json.get("emailAddress"));
-        System.out.print("De Date Values.....    "+new Date(json.get("dateOfBirth").asLong()));
 
         ClientDetails clientDetails = new ClientDetails();
         clientDetails.dateOfBirth = json.get("dateOfBirth").asLong();
@@ -120,8 +119,10 @@ public class Application extends Controller {
     public static Result uploadIdImage(){
         ObjectNode result = Json.newObject();
         final Map<String, String[]> values = request().body().asFormUrlEncoded();
+        String val = Form.form().
+                bindFromRequest().get("clientId");
 
-        ClientDetails clientDetails = ClientDetails.findByClientId(Long.parseLong(values.get("clientId")));
+        ClientDetails clientDetails = ClientDetails.findByClientId(Long.parseLong(val));
 
         Http.MultipartFormData body = request().body().asMultipartFormData();
         if(body == null){
@@ -132,7 +133,58 @@ public class Application extends Controller {
             if(file != null){
                 String myUploadPath = Play.application().configuration().getString("clientIdPhotosPath");
                 String photoName = clientDetails.nationalId+"."+extension;
-                file.renameTo(new File(myUploadPath, photoName));
+
+                try {
+                    Files.move(file.toPath(),new File(myUploadPath + File.separator + photoName).toPath());
+                } catch (IOException e) {
+                    try {
+                        Files.delete(new File(myUploadPath + File.separator + photoName).toPath());
+                        Files.move(file.toPath(),new File(myUploadPath + File.separator + photoName).toPath());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                        e.printStackTrace();
+                    }
+                }
+
+                clientDetails.nationalIdPhoto = photoName;
+                clientDetails.update();
+                result.put("status", "SUCCESS");
+            } else {
+                result.put("status", "file.upload.failed.parsing.error");
+            }
+        }
+        return ok(Json.toJson(result));
+    }
+
+    public static Result uploadProfileImage(){
+        ObjectNode result = Json.newObject();
+        final Map<String, String[]> values = request().body().asFormUrlEncoded();
+        String val = Form.form().
+                bindFromRequest().get("clientId");
+
+        ClientDetails clientDetails = ClientDetails.findByClientId(Long.parseLong(val));
+
+        Http.MultipartFormData body = request().body().asMultipartFormData();
+        if(body == null){
+            result.put("status", "Null body");
+        } else {
+            File file = body.getFiles().get(0).getFile();
+            String extension = body.getFiles().get(0).getContentType().split("image/")[1];
+            if(file != null){
+                String myUploadPath = Play.application().configuration().getString("clientPhotosPath");
+                String photoName = clientDetails.nationalId+"."+extension;
+
+                try {
+                    Files.move(file.toPath(),new File(myUploadPath + File.separator + photoName).toPath());
+                } catch (IOException e) {
+                    try {
+                        Files.delete(new File(myUploadPath + File.separator + photoName).toPath());
+                        Files.move(file.toPath(),new File(myUploadPath + File.separator + photoName).toPath());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                        e.printStackTrace();
+                    }
+                }
 
                 clientDetails.nationalIdPhoto = photoName;
                 clientDetails.update();
--- build.sbt
+++ build.sbt
@@ -17,6 +17,8 @@ libraryDependencies ++= Seq(
   "mysql" % "mysql-connector-java" % "5.1.21",
   "com.googlecode.json-simple" % "json-simple" % "1.1.1",
   "joda-time" % "joda-time" % "2.3",
-  "commons-io" % "commons-io" % "2.3")
+  "commons-io" % "commons-io" % "2.3",
+  "org.apache.commons" % "commons-io" % "1.3.2"
+)
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
